09a116369f3f4150260fcf2f954c27da75dfc11d
Pete Inge <pete.inge@gmail.com>
2024-11-04 13:19:48 -0500

    Initial setup for Entity reference field search

---
+++ src/Form/EntityReferenceFieldSearch.php
@@ -0,0 +1,386 @@
+<?php
+
+namespace Drupal\bluecadet_utilities\Form;
+
+use Drupal\bluecadet_utilities\DrupalStateTrait;
+use Drupal\Core\Entity\EntityFieldManager;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Link;
+use Drupal\Core\Messenger\MessengerTrait;
+
+/**
+ * Bluecadet Utility Settings Form.
+ */
+class EntityReferenceFieldSearch extends FormBase {
+
+  use DrupalStateTrait;
+  use MessengerTrait;
+
+  /**
+   * Drupal Module Handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandler
+   */
+  private $moduleHandler;
+
+  /**
+   * Drupal Entity Field Manager.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManager
+   */
+  private $entityFieldManager;
+
+  /**
+   * Drupal Entity Type Manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  private $entityTypeManager;
+
+  /**
+   * Get module handler.
+   */
+  private function moduleHandler() {
+    if (!$this->moduleHandler) {
+      $this->moduleHandler = \Drupal::service('module_handler'); // phpcs:ignore
+    }
+
+    return $this->moduleHandler;
+  }
+
+  /**
+   * Get Entity Field Manager.
+   */
+  private function entityFieldManager(): EntityFieldManager {
+    if (!$this->entityFieldManager) {
+      $this->entityFieldManager = \Drupal::service('entity_field.manager'); // phpcs:ignore
+    }
+    return $this->entityFieldManager;
+  }
+
+  /**
+   * Get Entity Type Manager.
+   */
+  private function entityTypeManager(): EntityTypeManager {
+    if (!$this->entityTypeManager) {
+      $this->entityTypeManager = \Drupal::entityTypeManager(); // phpcs:ignore
+    }
+    return $this->entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'bcu_search+ent_ref_fields';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $session_data = $_SESSION['bcu_ent_ref_search_results'] ?? [];
+
+    $form['#tree'] = TRUE;
+
+    // Build Entities Select Field.
+    $entity_opts = [];
+    foreach ($this->entityTypeManager()->getDefinitions() as $id => $ent_def) {
+      $entity_opts[$id] = $ent_def->getLabel();
+    }
+
+    $form['entity_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t("Entity Types"),
+      '#description' => $this->t("Choose the Entity type you want to search for"),
+      '#options' => $entity_opts,
+      '#default_value' => $session_data[1]['entity_type'] ?? "",
+    ];
+
+    // Search field.
+    $form['search'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t("Entity Id"),
+      '#step' => 1,
+      '#description' => $this->t("This is doing a full string search on the raw html of the text field values. You can use '%' as a wildcard. As most ids are ints, be careful."),
+      '#default_value' => $session_data[1]['search_str'] ?? "",
+    ];
+
+    // Actions.
+    $form['actions'] = ['#type' => 'actions'];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Search'),
+    ];
+
+    $striper = [
+      'transparent' => '#eeeeee',
+      '#eeeeee' => 'transparent',
+    ];
+    $current_stripe = "#eeeeee";
+
+    if (!empty($session_data)) {
+      $form['results'] = [
+        '#weight' => -1,
+        'timing' => [
+          '#markup' => "Results took " . $session_data[3],
+        ],
+        'results' => [],
+      ];
+
+      foreach ($session_data[1]['data'] as $entity_type => $data) {
+
+        $list = [
+          '#theme' => 'item_list',
+          '#title' => 'Results for: ' . $entity_type,
+          '#items' => [],
+        ];
+        $current_stripe = "#eeeeee";
+
+        foreach ($data as $id => $result_data) {
+
+          $link = Link::fromTextAndUrl($result_data['label'], $result_data['url']);
+          $list['#items'][] = [
+            '#wrapper_attributes' => [
+              'style' => "padding: 1em;background-color:" . $current_stripe,
+            ],
+            [
+              '#markup' => "<p>Found " . $this->formatPlural($result_data['count'], "1 time", "@count times") . " on " . $link->toString() . "</p>",
+            ],
+            [
+              '#theme' => 'item_list',
+              '#title' => 'Fields:',
+              '#items' => $result_data['fields'],
+            ],
+          ];
+          $current_stripe = $striper[$current_stripe];
+        }
+
+        $form['results']['results'][] = $list;
+      }
+    }
+
+    unset($_SESSION['bcu_ent_ref_search_results']);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+
+    $field_map = $this->entityFieldManager()->getFieldMap();
+
+    $field_type = [
+      "entity_reference",
+      "entity_reference_revisions",
+      "entity_reference_entity_modify",
+    ];
+
+    $fields = [];
+    foreach ($field_map as $entity => $entity_field_data) {
+      foreach ($entity_field_data as $field_id => $field_data) {
+        if (in_array($field_data['type'], $field_type)) {
+
+          $field_storage_defs = $this->entityFieldManager()->getFieldStorageDefinitions($entity);
+
+          if (isset($field_storage_defs[$field_id])) {
+            $field_storage_settings = $field_storage_defs[$field_id]->getSettings();
+            if ($field_storage_settings['target_type'] == $values['entity_type']) {
+              $fields[$field_data['type']][$entity][$field_id] = $field_data;
+            }
+          }
+        }
+      }
+    }
+
+    $batch = [
+      'title' => $this->t('Searching...'),
+      'operations' => [
+        [
+          [$this, 'setUpContext'],
+          [$values['search'], $values['entity_type']],
+        ],
+      ],
+      'finished' => [$this, 'finishedCallback'],
+    ];
+
+    foreach ($fields as $field_type => $field_data) {
+      foreach ($field_data as $entity_type => $fields2) {
+        foreach ($fields2 as $f => $fdata) {
+          $batch['operations'][] = [
+            [$this, 'searchEntityRefFields'],
+            [
+              $field_type,
+              $entity_type,
+              [
+                $f => $fdata,
+              ],
+              $values['search'],
+            ],
+          ];
+        }
+      }
+    }
+
+    $batch['operations'][] = [
+      [$this, 'processResults'],
+      [],
+    ];
+
+    batch_set($batch);
+  }
+
+  /**
+   * Batch process to setup the $context array.
+   */
+  public static function setUpContext($search_string, $entity_type, &$context) {
+    $context['results']['search_str'] = $search_string;
+    $context['results']['entity_type'] = $entity_type;
+    $context['results']['raw'] = [];
+    $context['results']['data'] = [];
+    $context['results']['errors'] = [];
+  }
+
+  /**
+   * Batch Search process for each entity and field type combo.
+   */
+  public static function searchEntityRefFields($field_type, $entity_type, $fields, $search_string, &$context) {
+    $query = \Drupal::entityQuery($entity_type)->accessCheck(FALSE);
+
+    $group = $query->orConditionGroup();
+
+    foreach ($fields as $field_id => $data) {
+      $group->condition($field_id, $search_string, 'LIKE');
+    }
+
+    $query->condition($group);
+    $r = $query->execute();
+
+    if (!empty($r)) {
+      $context['results']['raw'][$entity_type][$field_type][$field_id] = $r;
+    }
+  }
+
+  /**
+   * Process the results of all the queries.
+   */
+  public static function processResults(&$context) {
+
+    if (!empty($context['results']['raw'])) {
+      foreach ($context['results']['raw'] as $entity_type => $field_types) {
+        foreach ($field_types as $fields) {
+          foreach ($fields as $field_id => $results) {
+            $method_name = "processResults_" . $entity_type;
+            if (method_exists(__CLASS__, $method_name)) {
+              EntityReferenceFieldSearch::$method_name($results, $field_id, $context);
+            }
+            else {
+
+              $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+              $entities = $storage->loadMultiple($results);
+
+              foreach ($entities as $entity) {
+                try {
+                  if (in_array('canonical', $entity->uriRelationships())) {
+                    $data = $context['results']['data'][$entity_type][$entity->id()] ?? [
+                      'url' => $entity->toUrl(),
+                      'label' => $entity->label(),
+                      'count' => 0,
+                      'fields' => [],
+                    ];
+                    $data['count']++;
+
+                    if (!in_array($field_id, $data['fields'])) {
+                      $data['fields'][] = $field_id;
+                    }
+
+                    $context['results']['data'][$entity_type][$entity->id()] = $data;
+                  }
+                  else {
+                    $context['results']['errors'][] = "Cannot create link for " . $entity->label();
+                  }
+                }
+                catch (\Throwable $e) {
+                  $context['results']['errors'][] = $e->getMessage();
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // phpcs:disable Drupal.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
+
+  /**
+   * Process query results for paragraph entities.
+   *
+   * We have to seperate this out b/c we need to look for its parent entity to
+   * create a link to it.
+   */
+  public static function processResults_paragraph($results, $field_id, &$context) {
+    $storage = \Drupal::entityTypeManager()->getStorage('paragraph');
+    $entities = $storage->loadMultiple($results);
+
+    foreach ($entities as $entity) {
+      try {
+        $continue = TRUE;
+        $loop_count = 0;
+
+        $entity_clone = clone $entity;
+
+        while ($continue) {
+          $parent = $entity_clone->getParentEntity();
+
+          if ($parent && in_array('canonical', $parent->uriRelationships())) {
+            $continue = FALSE;
+          }
+
+          $entity_clone = $parent;
+          $loop_count++;
+          if ($loop_count > 100) {
+            $continue = FALSE;
+          }
+        }
+
+        $data = $context['results']['data']['paragraph'][$parent->id()] ?? [
+          'url' => $parent->toUrl(),
+          'label' => $parent->label(),
+          'count' => 0,
+          'fields' => [],
+        ];
+        $data['count']++;
+        if (!in_array($field_id, $data['fields'])) {
+          $data['fields'][] = $field_id;
+        }
+
+        $context['results']['data']['paragraph'][$parent->id()] = $data;
+      }
+      catch (\Throwable $e) {
+        $context['results']['errors'][] = $e->getMessage();
+      }
+    }
+  }
+
+  // phpcs:enable Drupal.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
+
+  /**
+   * Batch finished callback.
+   */
+  public static function finishedCallback($success, $results, $operations, $elapsed) {
+    // Just add all results to the session var to let the form render results.
+    $_SESSION['bcu_ent_ref_search_results'] = [
+      $success,
+      $results,
+      $operations,
+      $elapsed,
+    ];
+  }
+
+}
